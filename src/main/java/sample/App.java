a3d25cdd923cae26b14ab914faef06bbd692b405
yoogodong <yoogodong@gmail.com>
2020-07-06 22:41:55 +0800

    添加数据库初始化脚本

--- src/main/java/sample/App.java
+++ src/main/java/sample/App.java
@@ -1,11 +1,33 @@
 package sample;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
+@Slf4j
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+
+    @Bean
+    DataSourceInitializer initializer(DataSource dataSource) {
+
+        DataSourceInitializer initializer = new DataSourceInitializer();
+        initializer.setDataSource(dataSource);
+
+        ClassPathResource script = new ClassPathResource("schema.sql");
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator(script);
+        initializer.setDatabasePopulator(populator);
+
+        return initializer;
     }
 }

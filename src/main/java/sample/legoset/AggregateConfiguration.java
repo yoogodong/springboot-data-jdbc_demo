701af8b2edb982982376578c8f628d7998b339f7
yoogodong <yoogodong@gmail.com>
2020-07-08 12:55:28 +0800

    采用实体事件事件回调机制来设置实体主键

--- src/main/java/sample/legoset/AggregateConfiguration.java
+++ src/main/java/sample/legoset/AggregateConfiguration.java
@@ -1,13 +1,17 @@
 package sample.legoset;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.jdbc.core.convert.JdbcCustomConversions;
 import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration;
+import org.springframework.data.relational.core.conversion.AggregateChange;
+import org.springframework.data.relational.core.mapping.event.BeforeSaveCallback;
 import org.springframework.lang.Nullable;
 
 import java.sql.Clob;
 import java.sql.SQLException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static java.util.Arrays.asList;
 
@@ -18,23 +22,42 @@ import static java.util.Arrays.asList;
  */
 @Configuration
 public class AggregateConfiguration extends AbstractJdbcConfiguration {
-	@Override
-	public JdbcCustomConversions jdbcCustomConversions() {
+    final AtomicInteger id = new AtomicInteger(0);
 
-		return new JdbcCustomConversions(asList(new Converter<Clob, String>() {
-			@Nullable
-			@Override
-			public String convert(Clob clob) {
-				try {
-					return Math.toIntExact(clob.length()) == 0 //
-							? "" //
-							: clob.getSubString(1, Math.toIntExact(clob.length()));
-				} catch (SQLException e) {
-					throw new IllegalStateException("不能将Clob 转化成 String.", e);
-				}
-			}
-		}));
-	}
+    @Override
+    public JdbcCustomConversions jdbcCustomConversions() {
 
+        return new JdbcCustomConversions(asList(new Converter<Clob, String>() {
+            @Nullable
+            @Override
+            public String convert(Clob clob) {
+                try {
+                    return Math.toIntExact(clob.length()) == 0 //
+                            ? "" //
+                            : clob.getSubString(1, Math.toIntExact(clob.length()));
+                } catch (SQLException e) {
+                    throw new IllegalStateException("不能将Clob 转化成 String.", e);
+                }
+            }
+        }));
+    }
+
+    @Bean
+    public BeforeSaveCallback beforeSaving() {
+        return new BeforeSaveCallback<LegoSet>() {
+            @Override
+            public LegoSet onBeforeSave(LegoSet legoSet, AggregateChange<LegoSet> aggregateChange) {
+                if (legoSet.getId() == 0) {
+                    legoSet.setId(id.incrementAndGet());
+                }
+                Manual manual = legoSet.getManual();
+
+                if (manual != null) {
+                    manual.setId((long) legoSet.getId());
+                }
+                return legoSet;
+            }
+        };
+    }
 
 }
